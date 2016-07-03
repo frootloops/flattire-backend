d691066588f00fad3d14bb423c13f11a587346c0
Arsen Gasparyan <to.arsen.gasparyan@gmail.com>
2016-07-03 15:31:53 +0300

    Add access_token to DriverSerializer

--- app/serializers/api/v1/driver_serializer.rb
+++ app/serializers/api/v1/driver_serializer.rb
@@ -1,4 +1,8 @@
 class Api::V1::DriverSerializer < ActiveModel::Serializer
-  attributes :id, :phone, :name, :email
+  attributes :id, :phone, :name, :email, :access_token
+
+  def access_token
+    object.authentication_token
+  end
 end
 
