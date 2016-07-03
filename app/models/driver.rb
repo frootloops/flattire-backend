707e9bff6d6cf237e403703c9e05a146359e60d6
Arsen Gasparyan <to.arsen.gasparyan@gmail.com>
2016-07-03 15:11:06 +0300

    Add phone number validation

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,4 +1,6 @@
 class Driver < ApplicationRecord
   devise :database_authenticatable, authentication_keys: [:phone]
   has_one_time_password counter_based: true
+  validates_uniqueness_of :phone
+  validates :phone, phone: true
 end
--- spec/models/driver_spec.rb
+++ spec/models/driver_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Driver, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validation" do
+    it "number validation" do
+      driver = build(:driver, phone: "+79999808630")
+      expect(driver).to be_valid
+
+      driver.phone = "+1-800-MY-APPLE"
+      expect(driver).to_not be_valid
+    end
+  end
 end
